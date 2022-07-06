7c396e5ce73578681156071852197ddcc0f1c8d8
GulinAlexey <gulin-2001@mail.ru>
2022-07-07 03:46:00 +0700

    Реструктуризация файлов

--- pokerbot_app/main.cpp
+++ pokerbot_app/main.cpp
@@ -10,17 +10,20 @@ int main()
 {
     TgBot::Bot bot(bot_token); //объявление объекта телеграм-бота
 
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) 
+        {
         bot.getApi().sendMessage(message->chat->id, "Hi!");
         });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) 
+        {
         printf("User wrote %s\n", message->text.c_str());
         if (StringTools::startsWith(message->text, "/start")) {
             return;
         }
         bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
         });
-    try {
+    try 
+    {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
         TgBot::TgLongPoll longPoll(bot);
         while (true) {
@@ -28,7 +31,8 @@ int main()
             longPoll.start();
         }
     }
-    catch (TgBot::TgException& e) {
+    catch (TgBot::TgException& e) 
+    {
         printf("error: %s\n", e.what());
     }
     return 0;
