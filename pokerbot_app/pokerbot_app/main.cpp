7e895c6ac665680e58a3930d6cd0b58902aa3815
GulinAlexey <gulin-2001@mail.ru>
2022-07-06 20:26:00 +0700

    Добавлен пример кода для работы бота. Протестировано

--- pokerbot_app/pokerbot_app/main.cpp
+++ pokerbot_app/pokerbot_app/main.cpp
@@ -1,13 +1,35 @@
 ﻿#include <cstdio>
 #include <iostream>
-#include "tgbot-cpp/include/tgbot/Bot.h"
+#include <string>
+#include <tgbot/tgbot.h>
+#include "bot_token.h"
 
 using namespace std;
 
 int main()
 {
-    printf("Вас приветствует %s!\n", "pokerbot_app");
-    int n;
-    cin >> n;
+    TgBot::Bot bot(bot_token); //объявление объекта телеграм-бота
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
     return 0;
 }
