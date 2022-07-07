5bd6ad96b403c693783c2da9239a59512238761b
GulinAlexey <gulin-2001@mail.ru>
2022-07-08 00:50:47 +0700

    Добавлено чтение токена из файла, перевод на русский язык, вывод информации о событиях бота в текстовый файл лога

--- pokerbot_app/main.cpp
+++ pokerbot_app/main.cpp
@@ -1,39 +1,63 @@
 ﻿#include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <tgbot/tgbot.h>
-#include "bot_token.h"
+
+#define PATH_OF_TOKEN "data/token.dat" //путь к файлу, в котором хранится токен телеграм-бота
+#define PATH_OF_LOG "log.txt" //путь к файлу для записи лога
 
 using namespace std;
 
+ofstream outlog(PATH_OF_LOG); //поток вывода лога работы в файл
+
 int main()
 {
+    string bot_token; //токен бота
+    try
+    {
+        bot_token = FileTools::read(PATH_OF_TOKEN); //прочитать из файла
+    }
+    catch (ifstream::failure& e)
+    {
+        cout << "Отсутствует файл \"data/token.dat\" с токеном бота." << endl;
+        outlog << "Отсутствует файл \"data/token.dat\" с токеном бота." << endl;
+        return 0;
+    }
+
     TgBot::Bot bot(bot_token); //объявление объекта телеграм-бота
 
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) 
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) //при получении команды start
         {
         bot.getApi().sendMessage(message->chat->id, "Hi!");
         });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) 
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) //при получении любого сообщения
         {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
+        outlog << "Пользователь написал " << message->text.c_str() << endl;
+        if (StringTools::startsWith(message->text, "/start")) 
+        {
             return;
         }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        bot.getApi().sendMessage(message->chat->id, "Ваше сообщение: " + message->text);
         });
     try 
     {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        cout << "Имя бота: " << bot.getApi().getMe()->username.c_str() << endl;
+        cout << "Бот запущен" << endl;
+        outlog << "Имя бота: " << bot.getApi().getMe()->username.c_str() << endl;
+        outlog << "Бот запущен" << endl;
+
         TgBot::TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
+        while (true) //цикл длинных опросов для обработки сообщений пользователей
+        {
+            outlog << "Длинный опрос запущен" << endl;
             longPoll.start();
         }
     }
-    catch (TgBot::TgException& e) 
+    catch (TgBot::TgException& e) //при возникновении ошибки
     {
-        printf("error: %s\n", e.what());
+        cout << "Бот остановлен. Ошибка: " << e.what() << endl;
+        outlog << "Бот остановлен. Ошибка: "<< e.what() << endl;
     }
     return 0;
 }
